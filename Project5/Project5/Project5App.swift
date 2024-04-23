1328a6400ce529a0a29cf2863981d4ab3ab08fa0
chingizagha <cingiz167@gmail.com>
2024-04-24 01:41:56 +0400

    Project 5 Word Scramble App

---
+++ Project5/Project5/Project5App.swift
@@ -0,0 +1,17 @@
+//
+//  Project5App.swift
+//  Project5
+//
+//  Created by Chingiz on 23.04.24.
+//
+
+import SwiftUI
+
+@main
+struct Project5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
